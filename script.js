b86704f48109e86c709353506516e530e5dfc5b2
EFIELDS07 <field615@umn.edu>
2026-05-27 13:55:20 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -75,3 +75,133 @@ prevBtn.addEventListener("click", () => {
 
     showPage(current);
 });
+/* =========================================
+   PRINTER EFFECT
+========================================= */
+
+const invoice =
+document.querySelector(".invoice-paper");
+
+const invoiceObserver =
+new IntersectionObserver(entries => {
+
+    entries.forEach(entry => {
+
+        if(entry.isIntersecting){
+
+            invoice.classList.add("printing");
+
+            setTimeout(() => {
+
+                invoice.classList.add("stamped");
+
+            }, 1700);
+
+        }
+
+    });
+
+},{
+    threshold:.35
+});
+
+invoiceObserver.observe(invoice);
+
+/* =========================================
+   HISTORY BOX DROP
+========================================= */
+
+const historyBoxes =
+    document.querySelectorAll(".history-box");
+
+const historyObserver =
+new IntersectionObserver(entries => {
+
+    entries.forEach(entry => {
+
+        if(entry.isIntersecting){
+
+            historyBoxes.forEach((box,index) => {
+
+                setTimeout(() => {
+
+                    box.classList.add("drop-in");
+
+                }, index * 350);
+
+            });
+
+        }
+
+    });
+
+},{
+    threshold:.2
+});
+
+historyObserver.observe(
+    document.querySelector(".history-section")
+);
+
+/* =========================================
+   PRODUCE REVEAL
+========================================= */
+
+const crops =
+document.querySelectorAll(".crop-card");
+
+const sprout =
+document.querySelector(".horizon-sprout");
+
+const produceSection =
+document.querySelector(".produce-field-section");
+
+/* DIRT FALL */
+
+const cropObserver =
+new IntersectionObserver(entries => {
+
+    entries.forEach(entry => {
+
+        if(entry.isIntersecting){
+
+            entry.target.classList.add("grown");
+
+        }
+
+    });
+
+},{
+    threshold:.25
+});
+
+crops.forEach(crop => {
+
+    cropObserver.observe(crop);
+
+});
+
+/* HORIZON SPROUT */
+
+const sproutObserver =
+new IntersectionObserver(entries => {
+
+    entries.forEach(entry => {
+
+        if(entry.isIntersecting){
+
+            setTimeout(() => {
+
+                sprout.classList.add("grow");
+
+            }, 2500);
+
+        }
+
+    });
+
+},{
+    threshold:.35
+});
+
+sproutObserver.observe(produceSection);
